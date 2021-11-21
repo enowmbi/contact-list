7d91053380df75dcc47c9a17a093d5cc5b34907a
Enow Mbi <benowmbi@yahoo.com>
2021-11-21 21:20:21 +0100

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header =() =>{
+    return(
+        <div className ="container">
+        <div style={{textAlign: "center"}}>
+        <h1>Contact Manager</h1>
+        <hr style={{color: "red", height: "5px"}}/>
+        </div>
+        </div>
+    )
+}
+export default Header
+
