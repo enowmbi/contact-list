d5ea6523dcdeb4d684492a62b7e61703a737a0be
Enow Mbi <benowmbi@yahoo.com>
2021-11-21 21:21:42 +0100

    Add contact list component

---
+++ src/components/ContactList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ContactEntry from './ContactEntry'
+
+const ContactList =(props) =>{
+    const contacts = props.contacts
+
+    const RenderContactEntries = contacts.map((contact) =>{
+       return(
+         <ContactEntry key = {contact.id} id={contact.id} name={contact.name} email={contact.email}/>
+       )
+    })
+
+    return(
+        <div className="container">
+        <div style={{marginTop: "10px"}} >
+        <h3>Contact List</h3>
+        <ul className="list-group list-group-flush">
+        { RenderContactEntries }
+        </ul>
+        </div>
+        </div>
+    )
+}
+export default ContactList
