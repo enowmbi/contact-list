4314c403984480df55638061031117484a40d477
Enow Mbi <benowmbi@yahoo.com>
2021-11-21 21:21:22 +0100

    Add contact entry component

---
+++ src/components/ContactEntry.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'font-awesome/css/font-awesome.min.css';
+
+const ContactEntry =(props) =>{
+    const { id, name, email } = props
+    return(
+        <li className="list-group-item">
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+        <div><i className="fa fa-user fa-lg"></i></div>
+        <div style={{fontWeight: "bold"}}>{name}</div>
+        <div>{email}</div>
+        <div className="pull-right"><i className ="fa fa-trash fa-lg" style={{color: "red"}}></i></div>
+        </div>
+        </li>
+    )
+}
+
+export default ContactEntry
