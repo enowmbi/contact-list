ad7c9a94217ba519a058cbc359e40269579896d5
Enow Mbi <benowmbi@yahoo.com>
2021-11-22 12:45:05 +0100

    Add link to add contact component

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import ContactEntry from './ContactEntry'
 
 const ContactList =(props) =>{
@@ -21,7 +22,12 @@ const ContactList =(props) =>{
     return(
         <div className="container">
         <div style={{marginTop: "10px"}} >
+        <div style={{display: "flex", justifyContent: "space-evenly"}}>
         <h3>Contact List</h3>
+        <Link to="/add">
+        <button className="btn btn-primary pull-right">Add Contact</button>
+        </Link>
+        </div>
         <ul className="list-group list-group-flush">
         { RenderContactEntries }
         </ul>
