8873ff3b69c3cd0b0681cd6a486d4341f1079adc
Enow Mbi <benowmbi@yahoo.com>
2021-11-22 18:56:11 +0100

    Install json server and axios

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,7 +6,7 @@ const SearchForm =(props)=>{
     const [ searchText, setSearchText ] = useState("")
  return(
      <React.Fragment>
-     <div class="input-group mb-3">
+     <div class="input-group m-2">
      <input type="text" value={searchText} onChange ={(event) => {setSearchText(event.target.value)}} class="form-control" placeholder="Search contact" aria-label="Search contact" aria-describedby="button-addon2" />
      <div class="input-group-append">
      <button class="btn btn-outline-primary" type="button" id="button-addon2" onClick={()=>props.handleSearch(searchText)}><i className="fa fa-search"></i></button>
