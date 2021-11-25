df11a93da0b8b980351489e6f2c306f60a207ab8
Enow Mbi <benowmbi@yahoo.com>
2021-11-25 08:09:29 +0100

    Rename class to className in search component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,10 +6,10 @@ const SearchForm =(props)=>{
     const [ searchText, setSearchText ] = useState("")
  return(
      <React.Fragment>
-     <div class="input-group m-2">
-     <input type="text" value={searchText} onChange ={(event) => {setSearchText(event.target.value)}} class="form-control" placeholder="Search contact" aria-label="Search contact" aria-describedby="button-addon2" />
-     <div class="input-group-append">
-     <button class="btn btn-outline-primary" type="button" id="button-addon2" onClick={()=>props.handleSearch(searchText)}><i className="fa fa-search"></i></button>
+     <div className="input-group m-2">
+     <input type="text" value={searchText} onChange ={(event) => {setSearchText(event.target.value)}} className="form-control" placeholder="Search contact" aria-label="Search contact" aria-describedby="button-addon2" />
+     <div className="input-group-append">
+     <button className="btn btn-outline-primary" type="button" id="button-addon2" onClick={()=>props.handleSearch(searchText)}><i className="fa fa-search"></i></button>
      </div>
      </div>
      </React.Fragment>
